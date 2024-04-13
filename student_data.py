b3bb33f93a369a4adace9ef3782a33c56ec40891
Andrei Miron <victormiron.home@gmail.com>
2024-04-13 08:54:20 -0400

    new uploads

---
+++ student_data.py
@@ -0,0 +1,24 @@
+def get_student_data (last_name: str, first_name: str, student_id: int ) -> tuple:
+    """ 
+    Creating a function that return user name, first name and student.
+
+    Parameters:
+    a (str) : name
+    b (str) : first name
+    c (int) : student id
+
+
+    returns :
+    tuple : a touple with name, first name and student id 
+    """
+
+    return last_name, first_name, student_id
+
+# asking for user input
+a = input("Please enter name: ")
+b = input("Please enter your first name: ")
+c = input("Please enter your student_id: ")
+
+# calling the function
+student_data = get_student_data(a, b, c)
+print(student_data)
